347d5edbd033557375c5c22cb4c0a405fc3b6d2e
Michal Wasiak <michal.wasiak@hotmail.com>
2025-09-13 15:44:36 +1000

    Task 03 done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,34 @@
-import {files} from './data.js';
+import { files } from './data.js';
+
+const toBytes = ({ length = 0, unit = 'B' }) => {
+    if (unit === 'KB') {
+        return length * 1024;
+    }
+    if (unit === 'MB') {
+        return length * 1024 * 1024;
+    }
+    if (unit === 'GB') {
+        return length * 1024 * 1024 * 1024;
+    }
+    return length;
+    //   switch (unit) {
+    //     case 'KB': return length * 1024;
+    //     case 'MB': return length * 1024 * 1024;
+    //     case 'GB': return length * 1024 * 1024 * 1024;
+    //     default:   return length; 
+    //   }
+};
+
+const getFileSize = ({ size: { length = 0, unit = 'B' } = {} }) =>
+    toBytes({ length, unit });
+
+const calculateFilesSize = (arr) => {
+    let total = 0;
+    arr.forEach((file) => {
+        total += getFileSize(file);
+    });
+    return total;
+};
+
+console.log(`SUMA: ${calculateFilesSize(files)} B`);
 
