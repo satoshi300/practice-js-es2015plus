3e6c80bedf3d6dfa0d94b7026356a54b43de3c64
Michal Wasiak <michal.wasiak@hotmail.com>
2025-09-16 21:24:28 +1000

    Rmove constructor() form 'class extends User' in Admin.js file

--- 04/Admin.js
+++ 04/Admin.js
@@ -1,10 +1,6 @@
 import User from './User.js';
 
 export default class Admin extends User {
-    constructor({ login = '', password = '' }) {
-        super({ login, password });
-    }
-
     isPasswordCorrect() {
         return this.password.length >= 10;
     }
